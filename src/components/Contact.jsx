5bc8b48baa893a72f072590b7c8ec2286b4d9c55
Abrar <ahmedabrararian@gmail.com>
2025-02-11 08:39:49 +0600

    Fixed live site issue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <section id="contact" className="py-16 px-5">
         <div className="container mx-auto">
           <h2 className="text-4xl font-bold mb-6">Contact</h2>
-          <form className="grid grid-cols-1 gap-6">
+          {/* <form className="grid grid-cols-1 gap-6">
             <input
               type="text"
               placeholder="Your Name"
@@ -24,7 +24,9 @@ const Contact = () => {
             <button type="submit" className="btn btn-primary">
               Send Message
             </button>
-          </form>
+          </form> */}
+          <p>Email Address: ahmedabrarariyan16@gmail.com</p>
+          <p>Phone Number: +8801819996320</p>
         </div>
       </section>
     </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,7 +5,7 @@ const Project = () => {
   const { id } = useParams();
   const [projects, setProjects] = useState([]);
   useEffect(() => {
-    fetch("../../public/projects.json")
+    fetch("/projects.json")
       .then((res) => res.json())
       .then((data) => setProjects(data));
   }, []);
