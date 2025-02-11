777cf16f583a479d39e448f990bc4c8431ed0569
Abrar <ahmedabrararian@gmail.com>
2025-02-11 08:31:44 +0600

    Updated the portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const currentLoc = useLocation();
   return (
     <div className="sticky z-50 top-0">
       <nav className="navbar bg-black text-white px-8 sticky top-0 z-50">
@@ -11,47 +12,55 @@ const Navbar = () => {
             Ahmed Abrar Ariyan
           </Link>
         </div>
-        <div className="hidden lg:flex items-center">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex lg:hidden ml-3">
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button">
-              <div>
-                <FaBars className="text-dark"></FaBars>
+        <div>
+          {currentLoc.pathname === "/" ? (
+            <>
+              <div className="hidden lg:flex items-center">
+                <ul className="menu menu-horizontal px-1">
+                  <li>
+                    <a href="#about">About</a>
+                  </li>
+                  <li>
+                    <a href="#skills">Skills</a>
+                  </li>
+                  <li>
+                    <a href="#projects">Projects</a>
+                  </li>
+                  <li>
+                    <a href="#contact">Contact</a>
+                  </li>
+                </ul>
               </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-black"
-            >
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#skills">Skills</a>
-              </li>
-              <li>
-                <a href="#projects">Projects</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
+              <div className="flex lg:hidden ml-3">
+                <div className="dropdown dropdown-end">
+                  <div tabIndex={0} role="button">
+                    <div>
+                      <FaBars className="text-dark"></FaBars>
+                    </div>
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-black"
+                  >
+                    <li>
+                      <a href="#about">About</a>
+                    </li>
+                    <li>
+                      <a href="#skills">Skills</a>
+                    </li>
+                    <li>
+                      <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                      <a href="#contact">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
         </div>
       </nav>
     </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -15,11 +15,31 @@ const Project = () => {
   return (
     <div>
       {specificData ? (
-        <div className="text-center flex flex-col justify-center">
+        <div className="text-center flex flex-col justify-center p-5">
           <p className="text-2xl font-semibold mb-2">{specificData.name}</p>
-          <img className="h-40 w-40 mx-auto object-cover p-2 rounded-xl shadow" src={specificData.image} alt="" />
-          <p>Tech Stack Used: MongoDB, Express.js, React, Node.js (MERN)</p>
-          <p>{specificData.description}</p>
+          <img
+            className="h-40 w-40 mx-auto object-cover p-2 rounded-xl shadow"
+            src={specificData.image}
+            alt=""
+          />
+          <div className="grid gap-3 my-6">
+            <p>
+              <span className="font-semibold">Tech Stack Used:</span> MongoDB,
+              Express.js, React, Node.js (MERN)
+            </p>
+            <p>
+              <span className="font-semibold">Description:</span>{" "}
+              {specificData.description}
+            </p>
+            <p>
+              <span className="font-semibold">Challenges Faced:</span>{" "}
+              {specificData.challenges}
+            </p>
+            <p>
+              <span className="font-semibold">Future Plan:</span>{" "}
+              {specificData.future}
+            </p>
+          </div>
           <div>
             <a href={specificData?.liveLink} className="btn btn-primary mr-2">
               Live Link
